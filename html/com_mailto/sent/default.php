69bfa163ca92a03ba551138962b2ea612d1f6383
yenlinh000 <yenlinh.nc21@gmail.com>
2021-04-06 13:20:49 +0700

    Add files via upload

---
+++ html/com_mailto/sent/default.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package Joomla template Framework
+ * @author enginetemplates https://www.enginetemplates.com
+ * @copyright Copyright (c) enginetemplates
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
+*/
+
+defined ('_JEXEC') or die();
+
+?>
+<div class="p-2">
+	<a href="javascript: void window.close()" title="<?php echo JText::_('COM_MAILTO_CLOSE_WINDOW'); ?>" class="close" aria-label="Close">
+		<span aria-hidden="true">&times;</span>
+	</a>
+
+	<h2 class="mt-0">
+		<?php echo JText::_('COM_MAILTO_EMAIL_SENT'); ?>
+	</h2>
+</div>
